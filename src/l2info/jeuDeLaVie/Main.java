6ebe77b75b449af6637b108b02ac38eb5c110f84
Tennessy_Kolubako <ten.perso@gmail.com>
2013-04-02 00:38:49 +0200

    Structure de base de la gestion des arguments

--- src/l2info/jeuDeLaVie/Main.java
+++ src/l2info/jeuDeLaVie/Main.java
@@ -25,6 +25,42 @@ public class Main {
 
 		
 		//System.out.println(jeu.evaluer(10));
+	
+		if(args.length > 0){
+			if(args[0] == "-name"){
+				
+			}
+			else if(args[0] == "-h"){
+				
+			}
+			else if(args.length == 3 && args[0] == "-s"){
+				try{
+					int duree = Integer.parseInt(args[1]);
+				}catch(NumberFormatException e){
+					System.out.println("La durée doit être numerique");
+				}
+			}
+			else if(args.length == 3 && args[0] == "-c"){
+				try{
+					int duree = Integer.parseInt(args[1]);
+				}catch(NumberFormatException e){
+					System.out.println("La durée doit être numerique");
+				}
+			}
+			else if(args.length == 3 && args[0] == "-w"){
+				try{
+					int duree = Integer.parseInt(args[1]);
+				}catch(NumberFormatException e){
+					System.out.println("La durée doit être numerique");
+				}
+			}
+			else{
+				
+			}
+		}
+		else{
+			
+		}
 		
 	}
 
